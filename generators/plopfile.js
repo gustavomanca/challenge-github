bf3fe5e29de23f7b2a7c8bd3ddb276c0df76b162
Gustavo Manca <dev3@easycrm.com.br>
2022-07-04 23:34:52 -0300

    chore: setup plop to automate components creation

---
+++ generators/plopfile.js
@@ -0,0 +1,25 @@
+//eslint-disable-next-line
+module.exports = function (plop) {
+  plop.setGenerator('controller', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'controller name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      }
+    ]
+  })
+}
