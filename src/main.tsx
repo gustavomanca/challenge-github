c0b0c90574020858275a26d36ae8c186e1cce91a
Gustavo Manca <dev3@easycrm.com.br>
2022-07-05 00:11:09 -0300

    feat: wrap layout component outer app

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
+import Layout from './components/Layout'
 import { GlobalStyles, theme } from './styles'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
+      <Layout>
+        <GlobalStyles />
+        <App />
+      </Layout>
     </ThemeProvider>
   </React.StrictMode>
 )
